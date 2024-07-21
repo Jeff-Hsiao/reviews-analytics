98cf984446215632e49e031f1df6794075ec39ec
Jeff Hsiao <samy88188@gmail.com>
2024-07-21 13:55:22 +0800

    add fast list

--- read.py
+++ read.py
@@ -21,3 +21,19 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+#good = []
+#for d in data:
+#	if 'good' in d:
+#		good.append(d)
+good = [d for d in data if 'good' in d]#等於上面第25行~第28行code
+
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
+
+bad = ['bad' in d for d in data]#'bad' in d 會判斷true or false，所以會有100萬筆true or false
+print(bad)
+
+bad = []
+for d in data:
+	bad.append('bad' in d)
